cf3f00db041917655ff5b17bcbd092ac6494f0e4
Nerses Javahiryan <nerses.javahiryan@vineti.com>
2022-06-21 19:18:53 +0400

    Added Car class

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,18 +1,22 @@
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
+
 public class CarTest {
+
     Car testCar = new Car();
 
-    @Test
+    @Test(priority = 1)
     public void testSetMaxSpeedMph() {
         assertEquals(100, testCar.setMaxSpeedMph(125));
 
     }
 
-    @Test
+    @Test(priority = 2)
     public void testSetMaxSpeedKph() {
         assertEquals(160.9, testCar.setMaxSpeedKph(100));
     }
